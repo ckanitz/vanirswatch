2160e808c4ee31a8443775e706a003589a927ec3
Christopher Kanitz <ck@luehrsen-heinrich.de>
2016-04-01 11:20:33 +0200

    better JobClass Libraray class

--- VanirsWatch/reader/JobClasses.cs
+++ VanirsWatch/reader/JobClasses.cs
@@ -1,111 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace VanirsWatch.reader
 {
-    class JobClasses
+    public class JobClasses
     {
-
-        private static Dictionary<int, string> jobClasses; = new Dictionary<int, string>();
-
-        static JobClasses()
-        {
+        private static Dictionary<int, string> jobClasses = new Dictionary<int, string>
+        {            
             // first
-            jobClasses.Add(0, "Novice");
-            jobClasses.Add(1, "Swordman");
-            jobClasses.Add(2, "Mage");
-            jobClasses.Add(3, "Archer");
-            jobClasses.Add(4, "Acolyte");
-            jobClasses.Add(5, "Merchant");
-            jobClasses.Add(6, "Thief");
+            {0, "Novice"},
+            {1, "Swordman"},
+            {2, "Mage"},
+            {3, "Archer"},
+            {4, "Acolyte"},
+            {5, "Merchant"},
+            {6, "Thief"},
 
             //2-1
-            jobClasses.Add(7, "Knight");
-            jobClasses.Add(8, "Priest");
-            jobClasses.Add(9, "Wizard");
-            jobClasses.Add(10, "Blacksmith");
-            jobClasses.Add(11, "Hunter");
-            jobClasses.Add(12, "Assassin");
+            {7, "Knight"},
+            {8, "Priest"},
+            {9, "Wizard"},
+            {10, "Blacksmith"},
+            {11, "Hunter"},
+            {12, "Assassin"},
 
             //2-2
-            jobClasses.Add(14, "Crusader");
-            jobClasses.Add(15, "Monk");
-            jobClasses.Add(16, "Sage");
-            jobClasses.Add(17, "Rogue");
-            jobClasses.Add(18, "Alchemist");
-            jobClasses.Add(19, "Bard");
-            jobClasses.Add(20, "Dancer");
+            {14, "Crusader"},
+            {15, "Monk"},
+            {16, "Sage"},
+            {17, "Rogue"},
+            {18, "Alchemist"},
+            {19, "Bard"},
+            {20, "Dancer"},
 
             //extended
-            jobClasses.Add(23, "Super Novice");
-            jobClasses.Add(24, "Gunslinger");
-            jobClasses.Add(25, "Ninja");
-            jobClasses.Add(4046, "Taekwon");
-            jobClasses.Add(4047, "Star Gladiator");
-            jobClasses.Add(4049, "Soul Linker");
+            {23, "Super Novice"},
+            {24, "Gunslinger"},
+            {25, "Ninja"},
+            {4046, "Taekwon"},
+            {4047, "Star Gladiator"},
+            {4049, "Soul Linker"},
 
             //r first
-            jobClasses.Add(4001, "High Novice");
-            jobClasses.Add(4002, "High Sowrdman");
-            jobClasses.Add(4003, "High Mage");
-            jobClasses.Add(4004, "High Archer");
-            jobClasses.Add(4005, "High Acolyte");
-            jobClasses.Add(4006, "High Merchant");
-            jobClasses.Add(4007, "High Thief");
+            {4001, "High Novice"},
+            {4002, "High Sowrdman"},
+            {4003, "High Mage"},
+            {4004, "High Archer"},
+            {4005, "High Acolyte"},
+            {4006, "High Merchant"},
+            {4007, "High Thief"},
 
             //r 2-1
-            jobClasses.Add(4008, "Lord Knight");
-            jobClasses.Add(4009, "High Priest");
-            jobClasses.Add(4010, "High Wizard");
-            jobClasses.Add(4011, "Whitesmith");
-            jobClasses.Add(4012, "Sniper");
-            jobClasses.Add(4013, "Assassin Cross");
+            {4008, "Lord Knight"},
+            {4009, "High Priest"},
+            {4010, "High Wizard"},
+            {4011, "Whitesmith"},
+            {4012, "Sniper"},
+            {4013, "Assassin Cross"},
 
             //r 2-2
-            jobClasses.Add(4015, "Paladin");
-            jobClasses.Add(4016, "Champion");
-            jobClasses.Add(4017, "Professor");
-            jobClasses.Add(4018, "Stalker");
-            jobClasses.Add(4019, "Creator");
-            jobClasses.Add(4020, "Clown");
-            jobClasses.Add(4021, "Gypsy");
+            {4015, "Paladin"},
+            {4016, "Champion"},
+            {4017, "Professor"},
+            {4018, "Stalker"},
+            {4019, "Creator"},
+            {4020, "Clown"},
+            {4021, "Gypsy"},
 
             //b first
-            jobClasses.Add(4023, "Baby Novice");
-            jobClasses.Add(4024, "Baby Swordman");
-            jobClasses.Add(4025, "Baby Mage");
-            jobClasses.Add(4026, "Baby Archer");
-            jobClasses.Add(4027, "Baby Acolyte");
-            jobClasses.Add(4028, "Baby Merchant");
-            jobClasses.Add(4029, "Baby Thief");
+            {4023, "Baby Novice"},
+            {4024, "Baby Swordman"},
+            {4025, "Baby Mage"},
+            {4026, "Baby Archer"},
+            {4027, "Baby Acolyte"},
+            {4028, "Baby Merchant"},
+            {4029, "Baby Thief"},
 
             //b 2-1
-            jobClasses.Add(4030, "Baby Knight");
-            jobClasses.Add(4031, "Baby Priest");
-            jobClasses.Add(4032, "Baby Wizard");
-            jobClasses.Add(4033, "Baby Blacksmith");
-            jobClasses.Add(4034, "Baby Hunter");
-            jobClasses.Add(4035, "Baby Assassin");
+            {4030, "Baby Knight"},
+            {4031, "Baby Priest"},
+            {4032, "Baby Wizard"},
+            {4033, "Baby Blacksmith"},
+            {4034, "Baby Hunter"},
+            {4035, "Baby Assassin"},
 
             //b 2-2
-            jobClasses.Add(4037, "Baby Crusader");
-            jobClasses.Add(4038, "Baby Monk");
-            jobClasses.Add(4039, "Baby Sage");
-            jobClasses.Add(4040, "Baby Rogue");
-            jobClasses.Add(4041, "Baby Alchemist");
-            jobClasses.Add(4042, "Baby Bard");
-            jobClasses.Add(4043, "Baby Dancer");
+            {4037, "Baby Crusader"},
+            {4038, "Baby Monk"},
+            {4039, "Baby Sage"},
+            {4040, "Baby Rogue"},
+            {4041, "Baby Alchemist"},
+            {4042, "Baby Bard"},
+            {4043, "Baby Dancer"},
 
             //b extended
-            jobClasses.Add(4045, "Super Baby");
-        }
+            {4045, "Super Baby"}
+        };
 
-        public string getJobClass(int jobID)
+        public static string getJobClass(int jobID)
         {
-            return jobClasses[jobID];
+            return jobClasses.ContainsKey(jobID) ? jobClasses[jobID] : "Unknown";
         }
     }
 }
